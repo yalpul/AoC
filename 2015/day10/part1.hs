f94b252b57c1ac0d5d9dd0c1acd269aca272f9a1
yalpul <yalpul@gmail.com>
2018-01-16 23:37:57 +0300

    day 10

---
+++ 2015/day10/part1.hs
@@ -0,0 +1,7 @@
+import Data.List
+
+lookAndSay :: String -> String
+lookAndSay x = concatMap (\x -> show (length x) ++ [head x]) $ group x
+
+main :: IO ()
+main = print $ length $ foldr (.) id (replicate 40 lookAndSay) "1113122113"
